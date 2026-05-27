52289e63aa03083beb221c62248cfe1e29f0ec85
yonatbu-hue <yonatbu@ac.sce.ac.il>
2026-05-27 17:33:53 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -10,4 +10,35 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
 
     Return: העומס P בניוטון (float)
     """
-    # כתבו כאן את הקוד
+ def sigma_max(P):
+        """
+        Maximum stress according to the secant formula.
+        """
+
+        # Argument of secant function
+        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
+
+        # sec(x) = 1 / cos(x)
+        sec_theta = 1 / np.cos(theta)
+
+        return (P / A) * (1 + (e * c / r**2) * sec_theta)
+
+    def f(P):
+        """
+        Root function:
+        sigma_max(P) - allowable stress
+        """
+        return sigma_max(P) - sigma_allow
+
+    # Euler buckling load (used as upper bound)
+    I = A * r**2
+    P_euler = (np.pi**2 * E * I) / (L**2)
+
+    # Bounds for bisection
+    P_min = 1e-6
+    P_max = 0.99 * P_euler
+
+    # Numerical solution
+    P_critical = bisect(f, P_min, P_max, xtol=1e-6)
+
+    return P_critical
